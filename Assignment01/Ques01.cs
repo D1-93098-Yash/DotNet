6d9f701c70f82d44b5b77a01eb03dffee37ac484
patilyashsachin <patilyash1914@gmail.com>
2025-12-22 23:56:05 +0530

    Assignment01 Q1 Q2

---
+++ Assignment01/Ques01.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Assignment01
+{
+    public class Ques01
+    {
+        public static void Main1(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
---
+++ Assignment01/Ques02.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment01
+{
+    internal class Ques02
+    {
+        public static void Main(string[] args)
+        {
+            int a, b;
+            string choice;
+            Console.WriteLine("Enter two numbers: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter your choice (+, -, *, /): ");
+            choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "+":
+                    Console.WriteLine("Addition: " + (a + b));
+                    break;
+                case "-":
+                    Console.WriteLine("Subtraction: " + (a - b));
+                    break;
+                case "*":
+                    Console.WriteLine("Multiplication: " + (a * b));
+                    break;
+                case "/":
+                    Console.WriteLine("Division: " + (a / b));
+                    break;
+            }
+        }
+    }
+}
